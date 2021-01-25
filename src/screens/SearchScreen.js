8a932c4267f0655344357d8034bae512d267d115
brendino500 <brenda.ty@live.com>
2021-01-25 11:31:10 +0000

    error handling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,36 +1,42 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import SearchBar from "../components/SearchBar.js";
-import yelp from "../api/yelp";
+import React, { useState } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import SearchBar from '../components/SearchBar.js'
+import yelp from '../api/yelp'
+import { getActiveChildNavigationOptions } from 'react-navigation'
 
 const SearchScreen = () => {
-  const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
+  const [term, setTerm] = useState('')
+  const [results, setResults] = useState([])
+  const [errorMessage, setErrorMessage] = useState('')
 
   const searchApi = async () => {
-    const response = await yelp.get("/search", {
-      params: {
-        limit: 50,
-        term,
-        location: "London",
-      },
-    });
-    setResults(response.data.businesses);
-  };
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term,
+          location: 'London',
+        },
+      })
+      setResults(response.data.businesses)
+    } catch (err) {
+      setErrorMessage('Something went wrong')
+    }
+  }
 
   return (
     <View>
       <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
-      <Text>Search Screen</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 15,
   },
-});
+})
 
-export default SearchScreen;
+export default SearchScreen
